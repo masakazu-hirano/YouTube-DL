bfc11fa853f07146d69ab66574166bc8eb955a41
masakazu-hirano <63486456+masakazu-hirano@users.noreply.github.com>
2023-04-29 13:51:47 +0900

    Update GET_NotionDB_List.py

--- Scripts/Modules/GET_NotionDB_List.py
+++ Scripts/Modules/GET_NotionDB_List.py
@@ -17,12 +17,13 @@ def get_notion_list():
 
     loop_count = 0
     has_more = True
+    results = []
     while has_more:
         if loop_count == 0:
             video_id_list = requests.post(
                 f"https://api.notion.com/v1/databases/{os.getenv('NOTION_DATABASE_ID')}/query",
                 headers = set_notion_header(),
-                json = {'page_size': 100}
+                json = {'page_size': 100} # 取得件数の上限は100件
             ).text
         elif loop_count > 0:
             video_id_list = requests.post(
@@ -30,17 +31,21 @@ def get_notion_list():
                 headers = set_notion_header(),
                 json = {
                     'start_cursor': start_cursor,
-                    'page_size': 100
+                    'page_size': 100 # 取得件数の上限は100件
                 }
             ).text
 
         # TODO: ここに値を取得する処理を実装
 
-        if json.loads(video_id_list)['has_more']:
+        if json.loads(video_id_list)['has_more']: # Request（'page_size'） 以上の件数が取得された場合、'has_more' プロパティに True が返る。
             start_cursor = json.loads(video_id_list)['next_cursor']
         else:
             has_more = json.loads(video_id_list)['has_more']
+                # 'has_more' プロパティが False の場合、ループを終了
 
         loop_count += 1
 
     # TODO: ここに返却する Return を記述
+    # NotionDB プロパティの取得先
+        # ページID: json.loads(response.text)['results']['id']
+        # プロパティ: json.loads(response.text)['results']['properties']['プロパティ名']
